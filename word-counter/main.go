8ef7a6ec8225a4ae60e41e1cc33774114029def9
Abdullahi Yunus <abdoollahikbk@gmail.com>
2025-12-06 23:11:17 +0300

    attempt to read from stdin correctly

--- word-counter/main.go
+++ word-counter/main.go
@@ -4,19 +4,10 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
-	"os"
 )
 
 func main() {
-	filename := "test.txt"
-
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatalf("unable to open file: %v", err)
-	}
-
-	defer file.Close()
+	var filename string
 
 	c := flag.Bool("c", false, "count bytes")
 	l := flag.Bool("l", false, "count lines")
@@ -26,36 +17,36 @@ func main() {
 	flag.Parse()
 
 	if *c {
-		bytes := getCount(filename, bufio.ScanBytes)
+		bytes := getCount(bufio.ScanBytes)
 		fmt.Printf("%v %s", bytes, filename)
 		return
 	}
 
 	if *l {
-		lines := getCount(filename, bufio.ScanLines)
+		lines := getCount(bufio.ScanLines)
 
 		fmt.Printf("%v %s", lines, filename)
 		return
 	}
 
 	if *w {
-		words := getCount(filename, bufio.ScanWords)
+		words := getCount(bufio.ScanWords)
 
 		fmt.Printf("%v %s", words, filename)
 		return
 	}
 
 	if *m {
-		runes := getCount(filename, bufio.ScanRunes)
+		runes := getCount(bufio.ScanRunes)
 
 		fmt.Printf("%v %s", runes, filename)
 		return
 	}
 
-	bytes := getCount(filename, bufio.ScanBytes)
-	lines := getCount(filename, bufio.ScanLines)
-	words := getCount(filename, bufio.ScanWords)
-	runes := getCount(filename, bufio.ScanRunes)
+	bytes := getCount(bufio.ScanBytes)
+	lines := getCount(bufio.ScanLines)
+	words := getCount(bufio.ScanWords)
+	runes := getCount(bufio.ScanRunes)
 
 	fmt.Printf("%v %v %v %v %s", bytes, lines, words, runes, filename)
 }
--- word-counter/utils.go
+++ word-counter/utils.go
@@ -8,13 +8,28 @@ import (
 
 // getCount is a generalize function to get the actual number of
 // or counts of the element we are interested in.
-func getCount(filename string, scanType bufio.SplitFunc) int {
-	count := 0
+func getCount(scanType bufio.SplitFunc) int {
+	var (
+		file *os.File
+		err  error
+	)
 
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatalf("failed to open file: %v", err)
+	args := os.Args
+
+	switch {
+	case len(args) > 1:
+		filename := args[1]
+		file, err = os.Open(filename)
+		if err != nil {
+			log.Fatalf("open file err: %v", err)
+		}
+		defer file.Close()
+
+	default:
+		file = os.Stdin
 	}
+
+	count := 0
 
 	sc := bufio.NewScanner(file)
 	sc.Split(scanType)
