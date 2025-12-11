35dd585fb3365bce9113ab2e5c2b1296dccbad8f
Abdullahi Yunus <abdoollahikbk@gmail.com>
2025-12-11 21:20:11 +0100

    add simple parser for {} pair

--- json-parser/main.go
+++ json-parser/main.go
@@ -1,3 +1,47 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) < 1 {
+		log.Fatal("file not specified")
+	}
+
+	filename := args[0]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanBytes)
+
+	var firstChar string
+	var lastChar string
+
+	for scanner.Scan() {
+		if firstChar == "" {
+			firstChar = scanner.Text()
+		}
+		lastChar = scanner.Text()
+	}
+
+	if firstChar != "{" || lastChar != "}" {
+		log.Fatalf("invalid first char(%v) and last char(%v)", firstChar, lastChar)
+	}
+
+	fmt.Println("valid json")
+
+}
